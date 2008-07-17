33b5902cc162deac242c326e32b04f27b2063395
Shane Sveller <shanesveller@gmail.com>
2008-07-17 11:18:48 -0400

    Basic Artist layout stuff

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -2,4 +2,9 @@ class ArtistsController < ApplicationController
   make_resourceful do
     actions :all
   end
+  
+  def current_objects
+    @current_objects = Artist.paginate :page => params[:page], :order => "genre, name ASC"
+  end
+  
 end
